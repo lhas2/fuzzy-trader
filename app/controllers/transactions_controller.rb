459a75c730ce0564101f061a68768eba524ef87e
lhas <luizhrqas@gmail.com>
2020-05-20 20:02:17 -0300

    apply dynamic price for @transaction.crypto_price in transactions#create

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -5,7 +5,9 @@ class TransactionsController < ApplicationController
 
     def create
         @transaction = Transaction.new(transaction_params)
-        @transaction.crypto_price = "53000.00"
+        @current_price = Crypto::CurrentPriceService.execute
+        @crypto_price = @current_price.find{|item| item['symbol'] == @transaction.crypto_currency}
+        @transaction.crypto_price = BigDecimal(@crypto_price['priceUsd'])
         if @transaction.valid?
             @transaction.save
             redirect_to show_wallet_url, notice: "Your transaction was added. 🚀"
